0e7202b71f3c6321cd6e180941c3fb1eb60df27a
Rejo <rajorshim23@gmail.com>
2017-06-19 08:40:13 +0530

    this will be the base file on which the new indentation logic will be based

---
+++ indenter.py
@@ -0,0 +1,10 @@
+import os
+
+print 'hello\n'
+
+src_file = raw_input('Please Enter your source file name\n')
+tar_file = raw_input('Please Enter your target file name\n')
+
+print 'source : ', src_file
+print 'target : ', tar_file
+
