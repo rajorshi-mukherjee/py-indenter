ebbb33602041ff29b88248aa1d254fa83db557c3
Rejo <rajorshim23@gmail.com>
2017-06-19 22:10:59 +0530

    Okay this baiscally completes the module that I was thinking about. It is a small module that I will be using in production for indenting code. Only thing that needs to be kept in mind is that the indentation is based on # and { }. So in their absence the program will probably fail miserably. Also new and minor improvements can still be made, but I will get to that later on. This basically serves the actual designated utility of the module.
    Peace Out Homies!
    Rock it!!
    _\m/

--- indenter.py
+++ indenter.py
@@ -1,10 +1,48 @@
-import os
+__doc__ = ''' This module is used to perform indentation with four spaces.
+We created this module to ease the burden of indentation in a small enterprise system 
+where indenting will have to be done manually.
+We want to ease the burden of doing manual labour. 
+             Meh!
 
-print 'hello\n'
+             Automation Ka Zamana Hai Bhai..!
+'''
+def indentation():
+    indent_depth = 0
+    src_file_name = raw_input('Please Enter your source file name : ')
+    tar_file_name = raw_input('Please Enter your target file name : ')
 
-src_file = raw_input('Please Enter your source file name\n')
-tar_file = raw_input('Please Enter your target file name\n')
+    src_file = open(src_file_name, 'r')
+    tar_file = open(tar_file_name, 'w')
 
-print 'source : ', src_file
-print 'target : ', tar_file
+    data = src_file.readlines()
 
+    for items in data :
+
+        items = items.strip()
+
+        if len(items) > 0 :
+
+            if items[0] == '#' :
+                if items[1] == '{' :
+                    spaces = ' '*4*indent_depth
+                    items = spaces + items
+                    tar_file.writelines(items + '\n')
+                    indent_depth +=1
+                    continue
+
+                if items[1] == '}' :
+                    indent_depth -=1
+                    spaces = ' '*4*indent_depth
+                    items = spaces + items
+                    tar_file.writelines(items + '\n')
+                    continue
+
+        spaces = ' '*4*indent_depth
+        items = spaces + items
+        tar_file.writelines(items + '\n')
+
+    src_file.close()
+    tar_file.close()
+
+
+indentation()
